3f58aff982b77824d9b8c8f1e568a9e758e6c440
marklai1998 <marklai1998@pm.me>
2025-03-02 14:08:40 -0500

    chore: add prettier

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,27 @@
 /// <reference types="node"/>
 
-import { ErrorRequestHandler, Request, RequestHandler, Response } from "express";
+import { ErrorRequestHandler, Request, RequestHandler, Response } from 'express'
 
-export type Transform = (body: {}, request: Request, response: Response) => any;
-export type TransformAsync = (body: {}, request: Request, response: Response) => PromiseLike<any>;
-export type TransformHeader = (request: Request, response: Response) => any;
-export type TransformHeaderAsync = (request: Request, response: Response) => PromiseLike<any>;
+export type Transform = (body: {}, request: Request, response: Response) => any
+export type TransformAsync = (
+  body: {},
+  request: Request,
+  response: Response
+) => PromiseLike<any>
+export type TransformHeader = (request: Request, response: Response) => any
+export type TransformHeaderAsync = (
+  request: Request,
+  response: Response
+) => PromiseLike<any>
 export type TransformChunk = (
-    chunk: string | Buffer,
-    encoding: string | null,
-    request: Request,
-    response: Response,
-) => string | Buffer;
+  chunk: string | Buffer,
+  encoding: string | null,
+  request: Request,
+  response: Response
+) => string | Buffer
 
 export interface Options {
-    mungError: boolean;
+  mungError: boolean
 }
 
 /**
@@ -23,7 +30,7 @@ export interface Options {
  * @param options json options.
  * @return Middleware to transform the body
  */
-export function json(fn: Transform, options?: Options): RequestHandler;
+export function json(fn: Transform, options?: Options): RequestHandler
 
 /**
  * Asynchronously transform the JSON body of the response.
@@ -31,21 +38,21 @@ export function json(fn: Transform, options?: Options): RequestHandler;
  * @param options jsonAsync options.
  * @return Middleware to transform the body
  */
-export function jsonAsync(fn: TransformAsync, options?: Options): RequestHandler;
+export function jsonAsync(fn: TransformAsync, options?: Options): RequestHandler
 
 /**
  * Transform the HTTP headers of the response.
  * @param fn A transformation function.
  * @return Middleware to transform the headers
  */
-export function headers(fn: TransformHeader): RequestHandler;
+export function headers(fn: TransformHeader): RequestHandler
 
 /**
  * Asynchronously transform the HTTP headers of the response.
  * @param fn A transformation function.
  * @return Middleware to transform the headers
  */
-export function headersAsync(fn: TransformHeaderAsync): RequestHandler;
+export function headersAsync(fn: TransformHeaderAsync): RequestHandler
 
 /**
  * Transform chunks as they are written to the response
@@ -53,10 +60,10 @@ export function headersAsync(fn: TransformHeaderAsync): RequestHandler;
  * @param options Write options.
  * @return Middleware to transform chunks.
  */
-export function write(fn: TransformChunk, options?: Options): RequestHandler;
+export function write(fn: TransformChunk, options?: Options): RequestHandler
 
 /**
  * Override the default onError handler.
  * @see {ErrorRequestHandler}
  */
-export let onError: ErrorRequestHandler;
+export let onError: ErrorRequestHandler
