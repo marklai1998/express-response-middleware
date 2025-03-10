b509d5f07b1c0ddaf4d03e1a56fee777ca4ef8ae
marklai1998 <marklai1998@pm.me>
2025-03-10 00:45:33 -0400

    test: merge async json test

--- src/tests/json.test.ts
+++ src/tests/json.test.ts
@@ -13,111 +13,158 @@ describe('jsonMiddleware', () => {
     ;(json as any).foo.bar.hopefully.fails()
   }
 
-  it('should return the JSON result', async () => {
-    const server = express()
-      .use(jsonMiddleware(inspect))
-      .get('/', (_req, res) => res.status(200).json({ a: 'a' }).end())
-    const response = await request(server).get('/')
+  const noopAsync: Transform = (json, _req, _res) => {
+    return new Promise(resolve => {
+      resolve(json)
+    })
+  }
 
-    const expected = { a: 'a', inspected_by: 'me' }
+  const inspectAsync: Transform = (json, _req, _res) => {
+    return new Promise(resolve => {
+      resolve(json)
+    }).then((json: any) => {
+      json.inspected_by = 'me'
+      return json
+    })
+  }
 
-    expect(response.status).toStrictEqual(200)
-    expect(response.body).toStrictEqual(expected)
-    expect(response.headers['content-length']).toStrictEqual(
-      JSON.stringify(expected).length.toString()
-    )
-  })
+  const errorAsync: Transform = (json, _req, _res) => {
+    return new Promise(resolve => {
+      resolve(json)
+    }).then((json: any) => json.foo.bar.hopefully.fails())
+  }
 
-  it('should call callback with an error', async () => {
-    const server = express()
-      .use(jsonMiddleware(inspect))
-      .get('/', (_req, res) => res.status(404).json({ a: 'a' }).end())
-    const response = await request(server).get('/')
+  const noPermission: Transform = (_json, _req, res) => {
+    res.status(403).send('no permissions')
+  }
 
-    const expected = { a: 'a', inspected_by: 'me' }
+  const noPermissionAsync: Transform = (json, _req, res) => {
+    res.status(403).send('no permissions')
+    return Promise.resolve(json)
+  }
 
-    expect(response.status).toStrictEqual(404)
-    expect(response.body).toStrictEqual(expected)
-    expect(response.headers['content-length']).toStrictEqual(
-      JSON.stringify(expected).length.toString()
-    )
-  })
+  it.each([inspect, inspectAsync])(
+    'should return the JSON result',
+    async handler => {
+      const server = express()
+        .use(jsonMiddleware(handler))
+        .get('/', (_req, res) => res.status(200).json({ a: 'a' }).end())
+      const response = await request(server).get('/')
 
-  it('should return the JSON result from a res.send', async () => {
-    const server = express()
-      .use(jsonMiddleware(inspect))
-      .get('/', (_req, res) => res.status(200).send({ a: 'a' }).end())
-    const response = await request(server).get('/')
+      const expected = { a: 'a', inspected_by: 'me' }
 
-    let expected = { a: 'a', inspected_by: 'me' }
+      expect(response.status).toStrictEqual(200)
+      expect(response.body).toStrictEqual(expected)
+      expect(response.headers['content-length']).toStrictEqual(
+        JSON.stringify(expected).length.toString()
+      )
+    }
+  )
 
-    expect(response.status).toStrictEqual(200)
-    expect(response.body).toStrictEqual(expected)
-    expect(response.headers['content-length']).toStrictEqual(
-      JSON.stringify(expected).length.toString()
-    )
-  })
+  it.each([inspect, inspectAsync])(
+    'should call callback with an error',
+    async handler => {
+      const server = express()
+        .use(jsonMiddleware(handler))
+        .get('/', (_req, res) => res.status(404).json({ a: 'a' }).end())
+      const response = await request(server).get('/')
 
-  it('should return a as application/json', async () => {
-    const server = express()
-      .use(jsonMiddleware(noop))
-      .get('/', (_req, res) => res.status(200).json(42).end())
-    const response = await request(server).get('/')
+      const expected = { a: 'a', inspected_by: 'me' }
 
-    expect(response.status).toStrictEqual(200)
-    expect(response.text).toStrictEqual('42')
-    expect(response.headers).toStrictEqual(
-      expect.objectContaining({
-        'content-type': 'application/json; charset=utf-8',
-      })
-    )
-  })
+      expect(response.status).toStrictEqual(404)
+      expect(response.body).toStrictEqual(expected)
+      expect(response.headers['content-length']).toStrictEqual(
+        JSON.stringify(expected).length.toString()
+      )
+    }
+  )
 
-  it('should abort if a response is sent', async () => {
-    const server = express()
-      .use(
-        jsonMiddleware((_json, _req, res) => {
-          res.status(403).send('no permissions')
+  it.each([inspect, inspectAsync])(
+    'should return the JSON result from a res.send',
+    async handler => {
+      const server = express()
+        .use(jsonMiddleware(handler))
+        .get('/', (_req, res) => res.status(200).send({ a: 'a' }).end())
+      const response = await request(server).get('/')
+
+      let expected = { a: 'a', inspected_by: 'me' }
+
+      expect(response.status).toStrictEqual(200)
+      expect(response.body).toStrictEqual(expected)
+      expect(response.headers['content-length']).toStrictEqual(
+        JSON.stringify(expected).length.toString()
+      )
+    }
+  )
+
+  it.each([noop, noopAsync])(
+    'should return a as application/json',
+    async handler => {
+      const server = express()
+        .use(jsonMiddleware(handler))
+        .get('/', (_req, res) => res.status(200).json(42).end())
+      const response = await request(server).get('/')
+
+      expect(response.status).toStrictEqual(200)
+      expect(response.text).toStrictEqual('42')
+      expect(response.headers).toStrictEqual(
+        expect.objectContaining({
+          'content-type': 'application/json; charset=utf-8',
         })
       )
-      .get('/', (_req, res) => res.status(200).json({ a: 'a' }).end())
-    const response = await request(server).get('/')
+    }
+  )
 
-    expect(response.status).toStrictEqual(403)
-    expect(response.text).toStrictEqual('no permissions')
-    expect(response.headers).toStrictEqual(
-      expect.objectContaining({
-        'content-type': 'text/html; charset=utf-8',
-      })
-    )
-  })
+  it.each([noPermission, noPermissionAsync])(
+    'should abort if a response is sent',
+    async handler => {
+      const server = express()
+        .use(jsonMiddleware(handler))
+        .get('/', (_req, res) => res.status(200).json({ a: 'a' }).end())
+      const response = await request(server).get('/')
 
-  it('should 500 on a synchronous exception', async () => {
-    const errorHandler: ErrorRequestHandler = (err, _req, res, _next) =>
-      res.status(500).send(err.message).end()
-    const server = express()
-      .use(errorHandler)
-      .use(jsonMiddleware(error))
-      .get('/', (_req, res) => res.status(200).json({ a: 'a' }).end())
-
-    const response = await request(server).get('/')
-
-    expect(response.status).toStrictEqual(500)
-  })
-
-  it('should 500 on an asynchronous exception', async () => {
-    const errorHandler: ErrorRequestHandler = (err, _req, res, _next) =>
-      res.status(500).send(err.message).end()
-    const server = express()
-      .use(errorHandler)
-      .use(jsonMiddleware(error))
-      .get('/', (_req, res) => {
-        process.nextTick(() => {
-          res.status(200).json({ a: 'a' }).end()
+      expect(response.status).toStrictEqual(403)
+      expect(response.text).toStrictEqual('no permissions')
+      expect(response.headers).toStrictEqual(
+        expect.objectContaining({
+          'content-type': 'text/html; charset=utf-8',
         })
-      })
-    const response = await request(server).get('/')
+      )
+    }
+  )
 
-    expect(response.status).toStrictEqual(500)
-  })
+  it.each([error, errorAsync])(
+    'should 500 on a synchronous exception',
+    async handler => {
+      const errorHandler: ErrorRequestHandler = (err, _req, res, _next) =>
+        res.status(500).send(err.message).end()
+      const server = express()
+        .use(errorHandler)
+        .use(jsonMiddleware(handler))
+        .get('/', (_req, res) => res.status(200).json({ a: 'a' }).end())
+
+      const response = await request(server).get('/')
+
+      expect(response.status).toStrictEqual(500)
+    }
+  )
+
+  it.each([error, errorAsync])(
+    'should 500 on an asynchronous exception',
+    async handler => {
+      const errorHandler: ErrorRequestHandler = (err, _req, res, _next) =>
+        res.status(500).send(err.message).end()
+      const server = express()
+        .use(errorHandler)
+        .use(jsonMiddleware(handler))
+        .get('/', (_req, res) => {
+          process.nextTick(() => {
+            res.status(200).json({ a: 'a' }).end()
+          })
+        })
+      const response = await request(server).get('/')
+
+      expect(response.status).toStrictEqual(500)
+    }
+  )
 })
