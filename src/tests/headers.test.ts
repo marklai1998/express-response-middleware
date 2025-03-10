b19b01092a2c1d992497e512a52607729229618e
marklai1998 <marklai1998@pm.me>
2025-03-10 00:47:56 -0400

    test: merge test

--- src/tests/headers.test.ts
+++ src/tests/headers.test.ts
@@ -1,16 +1,32 @@
 import express from 'express'
 import request from 'supertest'
 import { expect } from 'vitest'
-import { headersMiddleware } from '../main'
+import { headersMiddleware, TransformHeader } from '../main'
 
 describe('headers', () => {
-  it('should return the headers', async () => {
+  const header: TransformHeader = (_req, res) => {
+    res.set('x-inspected-by', 'me')
+  }
+
+  const headerAsync: TransformHeader = (_req, res) => {
+    return Promise.resolve(true).then(() => {
+      res.set('x-inspected-by', 'me')
+    })
+  }
+
+  const error: TransformHeader = (req, _res) => {
+    ;(req as any).hopefully_fails()
+  }
+
+  const errorAsync: TransformHeader = (req, _res) => {
+    return Promise.resolve(true).then(() => {
+      ;(req as any).hopefully_fails()
+    })
+  }
+
+  it.each([header, headerAsync])('should return the headers', async handler => {
     const server = express()
-      .use(
-        headersMiddleware((_req, res) => {
-          res.set('x-inspected-by', 'me')
-        })
-      )
+      .use(headersMiddleware(handler))
       .get('/', (_req, res) => res.status(200).json({ a: 'a' }).end())
     const response = await request(server).get('/')
 
@@ -23,52 +39,15 @@ describe('headers', () => {
     )
   })
 
-  it('should work with promises', async () => {
-    const server = express()
-      .use(
-        headersMiddleware((_req, res) => {
-          return Promise.resolve(true).then(() => {
-            res.set('x-inspected-by', 'me')
-          })
-        })
-      )
-      .get('/', (_req, res) => res.status(200).json({ a: 'a' }).end())
-    const response = await request(server).get('/')
+  it.each([error, errorAsync])(
+    'should 500 on a synchronous exception',
+    async handler => {
+      const server = express()
+        .use(headersMiddleware(handler))
+        .get('/', (_req, res) => res.status(200).json({ a: 'a' }).end())
+      const response = await request(server).get('/')
 
-    expect(response.status).toStrictEqual(200)
-    expect(response.body).toStrictEqual({ a: 'a' })
-    expect(response.headers).toStrictEqual(
-      expect.objectContaining({
-        'x-inspected-by': 'me',
-      })
-    )
-  })
-
-  it('should 500 on a synchronous exception', async () => {
-    const server = express()
-      .use(
-        headersMiddleware((req, _res) => {
-          ;(req as any).hopefully_fails()
-        })
-      )
-      .get('/', (_req, res) => res.status(200).json({ a: 'a' }).end())
-    const response = await request(server).get('/')
-
-    expect(response.status).toStrictEqual(500)
-  })
-
-  it('should 500 on an asynchronous exception', async () => {
-    const server = express()
-      .use(
-        headersMiddleware((req, _res) => {
-          return Promise.resolve(true).then(() => {
-            ;(req as any).hopefully_fails()
-          })
-        })
-      )
-      .get('/', (_req, res) => res.status(200).json({ a: 'a' }).end())
-    const response = await request(server).get('/')
-
-    expect(response.status).toStrictEqual(500)
-  })
+      expect(response.status).toStrictEqual(500)
+    }
+  )
 })
